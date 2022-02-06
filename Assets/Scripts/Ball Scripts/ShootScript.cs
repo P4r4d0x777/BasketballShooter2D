7416974267073f3139497c9c3a695cf65e7ef66f
P4r4d0x777 <iverbitskiy321@gmail.com>
2022-02-06 14:17:46 +0300

    update shootScript

--- Assets/Scripts/Ball Scripts/ShootScript.cs
+++ Assets/Scripts/Ball Scripts/ShootScript.cs
@@ -32,4 +32,79 @@ public class ShootScript : MonoBehaviour
         myCollider = GetComponent<Collider2D>();
     }
 
+    private void Start()
+    {
+        Dots = GameObject.Find("dots");
+
+        myBody.isKinematic = true;
+
+        myCollider.enabled = false;
+
+        // ?
+        startPosition = transform.position;
+
+        dotsPath = Dots.transform.Cast<Transform>().ToList().ConvertAll(t => t.gameObject);
+
+        for(int i = 0; i < dotsPath.Count; i++)
+        {
+            dotsPath[i].GetComponent<Renderer>().enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        Aim();
+    }
+    void Aim()
+    {
+        if (shoot)
+            return;
+
+        if(Input.GetAxis("Fire 1") == 1)
+        {
+            if(!aiming)
+            {
+                aiming = true;
+
+                startPosition = Input.mousePosition;
+
+                CalculatePath();
+            }
+        }
+    }
+
+    Vector2 GetForce(Vector3 mouse)
+    {
+        return (new Vector2(startPosition.x, startPosition.y) - new Vector2(mouse.x, mouse.y)) * power;
+    }
+
+    bool inDeadZone(Vector2 mouse)
+    {
+        return false;
+    }
+
+    bool inReleaseZone(Vector2 mouse)
+    {
+        return false;
+    }
+
+    void CalculatePath()
+    {
+        Vector2 velocity = GetForce(Input.mousePosition) * Time.fixedDeltaTime / myBody.mass;
+    }
+
+    Vector2 PathPoint(Vector2 StartP, Vector2 StartVel, float t)
+    {
+        return Vector2.zero;
+    }
+
+    void HidePath()
+    {
+
+    }
+
+    void ShowPath()
+    {
+
+    }
 }
