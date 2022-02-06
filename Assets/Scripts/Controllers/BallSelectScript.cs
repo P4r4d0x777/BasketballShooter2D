954285fdf749e98b5f20338da6c177baf77b7f1c
P4r4d0x777 <iverbitskiy321@gmail.com>
2022-02-06 12:26:49 +0300

    add ballselectscript

---
+++ Assets/Scripts/Controllers/BallSelectScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class BallSelectScript : MonoBehaviour
+{
+    private List<Button> buttons = new List<Button>();
+
+    private void Awake()
+    {
+        GetButtonsAndAddListeners();
+    }
+
+    void GetButtonsAndAddListeners()
+    {
+        GameObject[] allButtons = GameObject.FindGameObjectsWithTag("MenuBall");
+
+        for(int i = 0; i < allButtons.Length; i++)
+        {
+            buttons.Add(allButtons[i].GetComponent<Button>());
+            buttons[i].onClick.AddListener(()=>SelectABall());
+        }
+    }
+
+    public void SelectABall()
+    {
+        int index = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+
+        // inform GameManager which index is selected 
+    }
+}
